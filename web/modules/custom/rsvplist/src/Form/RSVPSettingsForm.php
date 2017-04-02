9ab6e523324809132fff212e2444ffd801d37d8d
Syed Huda <syed.huda@gmail.com>
2017-04-02 15:49:27 +0100

    Created config form for rsvp config page.

---
+++ web/modules/custom/rsvplist/src/Form/RSVPSettingsForm.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\rsvplist\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\NodeType;
+
+/**
+ * Class RSVPSettingsForm.
+ *
+ * @package Drupal\rsvplist\Form
+ */
+class RSVPSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'rsvplist.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'rsvplist_admin_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $types = NodeType::loadMultiple();
+    $config = $this->config('rsvplist.rsvpsettings');
+    $form['rsvplist_types'] = [
+      '#type' => 'checkboxes',
+      '#title' => $this->t('The content types to enable RSVP collection for'),
+      '#description' => $this->t('On the specified node types, an RSVP option will be available and can be enabled while that node is being editied.'),
+      '#options' => $types,
+      '#default_value' => $config->get('allowed_types'),
+    ];
+    $form['array_filter'] = ['#type' => 'value', '#value' => true];
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $allowed_types = array_filter($form_state->getValue('rsvplist_types'));
+    sort($allowed_types);
+    $this->config('rsvplist.settings')
+      ->set('allowed_types', $allowed_types)
+      ->save();
+    parent::submitForm($form, $form_state);
+  }
+}
