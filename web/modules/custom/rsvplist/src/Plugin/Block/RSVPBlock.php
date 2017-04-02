1c292f97359fea6f9a5700cf9c5eddff59139c8d
Syed Huda <syed.huda@gmail.com>
2017-04-02 15:07:21 +0100

    Set permission on rsvp block and form content.

--- web/modules/custom/rsvplist/src/Plugin/Block/RSVPBlock.php
+++ web/modules/custom/rsvplist/src/Plugin/Block/RSVPBlock.php
@@ -2,7 +2,12 @@
 
 namespace Drupal\rsvplist\Plugin\Block;
 
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Annotation\Translation;
+use Drupal\Core\Block\Annotation\Block;
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\node\Entity\Node;
 
 /**
  * Provides a 'RSVPBlock' block.
@@ -18,10 +23,17 @@ class RSVPBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-    $build = [];
-    $build['rsvpblock']['#markup'] = 'Implement RSVPBlock.';
+    return \Drupal::formBuilder()->getForm('\Drupal\rsvplist\Form\RSVPForm');
+  }
 
-    return $build;
+  public function blockAccess(AccountInterface $account) {
+    /** @var Node $node */
+    $node = \Drupal::routeMatch()->getParameter('node');
+    $nid = $node->nid->value;
+    if (is_numeric($nid)) {
+      return AccessResult::allowedIfHasPermission($account, 'view rsvplist');
+    }
+    return AccessResult::forbidden();
   }
 
 }
